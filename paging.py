48e99973bfdc6c7f32885703c1f1bcb57b93e84d
Tino <tinomuzambi@gmail.com>
2020-05-17 00:53:04 +0200

    Adds if name == main check.

--- paging.py
+++ paging.py
@@ -22,3 +22,10 @@ def main():
     print("FIFO", FIFO(size, pages), "page faults.")
     print("LRU", LRU(size, pages), "page faults.")
     print("OPT", OPT(size, pages), "page faults.")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python paging.py [number of pages]")
+    else:
+        main()
